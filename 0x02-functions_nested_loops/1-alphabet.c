aaebd1c0cd02b4d7e20d631af0a3897ed148081c
Yusuf Yahaya <feezynharmony@gmail.com>
2023-03-21 20:41:02 -0700

    git add

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet -  A function that prints alphabet in lower case
+ *
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
