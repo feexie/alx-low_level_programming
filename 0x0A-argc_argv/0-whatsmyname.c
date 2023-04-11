803bade2e5e621bfd3cdf194c937833603d36a07
Yusuf Yahaya <feezynharmony@gmail.com>
2023-04-11 14:26:56 -0700

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    /* Mark argc as unused to avoid the warning */
+    (void)argc;
+    
     /* argv[0] contains the name of the program */
     printf("%s\n", argv[0]);
+    
     return 0;
 }
