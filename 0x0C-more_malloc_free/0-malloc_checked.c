037b4155fd5259c38e1653e6368f270e141201d0
Yusuf Yahaya <feezynharmony@gmail.com>
2023-04-14 18:24:16 -0700

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,14 +10,13 @@
  *         with a status value of 98
  */
 void *malloc_checked(unsigned int b)
-	{
+{
         void *ptr;
-
+	
 	ptr = malloc(b);
-
         if (!ptr)
-        {
+        
                 exit(98);
-        }
+        
         return (ptr);
 }
