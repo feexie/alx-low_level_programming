bf11ad8137d4b3c78dd9bc258cfb3aa8499801c5
Yusuf Yahaya <feezynharmony@gmail.com>
2023-04-20 16:14:35 -0700

    git add

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "function_pointers.h"
+
 /**
  * array_iterator - function to iterate through arrays
  * @array: pointer to array of int type
  * @size: type pf size_t typedef
  * @action: pointer to function
- * Return: sucess
+ * Return: success
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
