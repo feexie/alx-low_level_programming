1bcfeab77d7b4d4e6e32e50b98773f84a59acd42
Yusuf Yahaya <feezynharmony@gmail.com>
2023-04-27 10:18:13 -0700

    debugging ads_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,39 +3,50 @@
 #include <stdlib.h>
 
 /**
- * add_node_end - adds a new node at the end of a list_t list
- * @head: pointer to pointer to the head node
- * @str: string to be added to the new node
+ * add_node_end - Adds a new node at the end of a list_t.
+ * @head: A pointer to the head of the list.
+ * @str: The string to add.
  *
- * Return: the address of the new element, or NULL if it failed
+ * Return: The address of the new element, or NULL if it failed.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node = malloc(sizeof(list_t));
-    list_t *last_node = *head;
+        int len = 0;
+        list_t *new_node, *last_node;
 
-    if (new_node == NULL)
-        return (NULL);
+        new_node = malloc(sizeof(list_t));
+        if (new_node == NULL)
+                return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+        if (str == NULL)
+        {
+                free(new_node);
+                return (NULL);
+        }
 
-    new_node->next = NULL;
+        while (str[len])
+                len++;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+        new_node->str = strdup(str);
+        if (new_node->str == NULL)
+        {
+                free(new_node);
+                return (NULL);
+        }
 
-    while (last_node->next != NULL)
-        last_node = last_node->next;
+        new_node->len = len;
+        new_node->next = NULL;
 
-    last_node->next = new_node;
+        if (*head == NULL)
+        {
+                *head = new_node;
+                return (*head);
+        }
 
-    return (new_node);
+        last_node = *head;
+        while (last_node->next != NULL)
+                last_node = last_node->next;
+
+        last_node->next = new_node;
+        return (*head);
 }
