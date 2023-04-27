f297c20b63174aa0d398c809141aa9fa0348626a
Yusuf Yahaya <feezynharmony@gmail.com>
2023-04-27 07:18:02 -0700

    0 task fix with do while loop

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,18 @@
 
 size_t print_list(const list_t *h)
 {
-    if (h == NULL)
-        return 0;
+    size_t count = 0;
 
-    if (h->str == NULL)
-        printf("[0] (nil)\n");
-    else
-        printf("[%d] %s\n", h->len, h->str);
+    do {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%d] %s\n", h->len, h->str);
 
-    return 1 + print_list(h->next);
+        h = h->next;
+        count++;
+    } while (h != NULL);
+
+    return count;
 }
 
