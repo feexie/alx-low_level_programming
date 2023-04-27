c28ef6fd46e91f98c054d2b8a213943fd1c50338
Yusuf Yahaya <feezynharmony@gmail.com>
2023-04-27 08:59:46 -0700

    fixed length list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,6 +5,7 @@
  * list_len - Returns the number of elements in a linked list_t list.
  * @h: A pointer to the head of the list_t list.
  * Return: The number of element in linked list
+ */
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
