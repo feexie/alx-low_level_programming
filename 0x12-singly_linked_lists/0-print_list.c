7328874d099714e7d00c3434d9957a4ceaf8fdd0
Yusuf Yahaya <feezynharmony@gmail.com>
2023-04-28 09:48:53 -0700

    fix zero tas

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,26 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: the list to print
- * Return: the number of nodes
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the head of the linked list
+ *
+ * Return: the number of nodes in the linked list
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-	const list_t *current = h;
+	size_t num_nodes = 0;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		if (current->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", current->len, current->str);
-
-		count++;
-		current = current->next;
+		printf("[%d] %s\n", h->len, h->str == NULL ? "(nil)" : h->str);
+		num_nodes++;
+		h = h->next;
 	}
 
-	return (count);
+	return (num_nodes);
 }
