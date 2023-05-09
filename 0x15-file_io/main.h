da71016bb2c698e2e1ef422dccbe37b451b47a00
Yusuf Yahaya <feezynharmony@gmail.com>
2023-05-08 21:41:30 -0700

    git add

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,20 @@
+#ifndef IO_
+#define IO_
+#define BUF_SIZE 1024
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+int _strlen(const char *s);
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int cp(const char *file_from, const char *file_to);
+
+#endif
