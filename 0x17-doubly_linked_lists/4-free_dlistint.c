9b3226e47ec079295acefdabd1dfc42eed558e16
feexie <feezynharmony@gmail.com>
2023-09-15 16:58:42 +0100

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/* Implementation of free_dlistint function */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *temp;
+
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
