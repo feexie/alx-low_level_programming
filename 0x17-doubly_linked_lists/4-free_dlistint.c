3715638bd1a350fc19454ee7db7f2c2f3f60b362
yusuf <feezynharmony@gmail.com>
2023-09-16 01:52:43 +0100

    fix

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -4,12 +4,12 @@
 /* Implementation of free_dlistint function */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
+	dlistint_t *temp;
 
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }
